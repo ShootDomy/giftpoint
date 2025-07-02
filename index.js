3f14761d8b1f44802dfd2d8f601c533c10042f20
ShootDomy <canizaresdomenica4@gmail.com>
2025-07-02 10:08:19 -0500

    Agregar rutas para gestionar usuarios: obtener, crear, actualizar y eliminar

--- index.js
+++ index.js
@@ -30,6 +30,54 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// Obtener Todos los usuarios
+app.get("/users", (req, res) => {
+  const data = readData();
+  res.json(data.users);
+});
+
+// Obtener un usuario por Id
+app.get("/users/:id", (req, res) => {
+  const data = readData();
+  const user = data.users.find((user) => user.uuid === req.params.id);
+  res.json(user);
+});
+
+// Crear un usuario
+app.post("/users", (req, res) => {
+  const data = readData();
+  const body = req.body;
+  const uuid = uuidv4();
+  console.log("BODYY=====>> ", req.body);
+  const user = {
+    uuid,
+    ...body,
+  };
+
+  data.users.push(user);
+  writeData(data);
+  res.json(user);
+});
+
+// Actualizar un usuario
+app.put("/users/:id", (req, res) => {
+  const data = readData();
+  const user = data.users.find((user) => user.uuid === req.params.id);
+  user.email = req.body.email;
+  user.password = req.body.password;
+  writeData(data);
+  res.json(user);
+});
+
+// Eliminar un usuario
+app.delete("/users/:id", (req, res) => {
+  const data = readData();
+  const user = data.users.find((user) => user.uuid === req.params.id);
+  data.users = data.users.filter((user) => user.uuid !== req.params.id);
+  writeData(data);
+  res.json(user);
+});
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}!`);
 });
