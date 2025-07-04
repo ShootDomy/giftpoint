b6f8a2ff19ea50cec2f652ceee0dde540ece7f11
ShootDomy <canizaresdomenica4@gmail.com>
2025-07-04 01:24:51 -0500

    feat: agrega validaciones en el registro de usuarios para email y contraseña

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,22 @@
 import { loginUsuario, registrarUsuario } from "../services/auth.service.js";
 
 export const registrar = async (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).json({ error: "Faltan campos requeridos" });
+  }
+
+  // Validar formato de email
+  if (!/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(req.body.email)) {
+    return res.status(400).json({ error: "Formato de email inválido" });
+  }
+
+  // Validar contraseña
+  if (req.body.password.length < 6) {
+    return res
+      .status(400)
+      .json({ error: "La contraseña debe tener 6 caracteres o más" });
+  }
+
   try {
     const user = await registrarUsuario(req.body);
 
